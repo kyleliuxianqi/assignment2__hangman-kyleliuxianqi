52fcac27ca427a959e74428619bf5ce9263cc1b8
kyleliuxianqi <liuxianqi1997@qq.com>
2021-10-16 03:05:19 +0200

    final version LIU Xianqi 61551

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-"# assignment2__hangman-kyleliuxianqi" 
-test
+Assignment2-Hangman
+by LIU Xianqi 61551
+
+1.About the words file and data preparation
+I got the inspiration from a real game 'Hangman' in Apple store.
+Then I put the word that I didn't find out into the 'words_and_clues.csv', which follow the format as [word,clue].
+Thus, I use a hashmap to store them, which make words as keys and make their clues as values.
+
+2.Game begin
+- The number of '_' represents the length of the hidden word.
+- Above the hidden word, there's a clue help user to take a guess.
+- Under the hidden word, there's text displaying how many lives left(10 left at the beginning).
+- The bottom of the activity is the keyboard designed and created by me.
+  Idea: I store 'a-z' as array in the resource, then dynamically generate them in the JAVA code.
+- During the game, if user click the wrong letter, the picture above will change.
+  Besides, the clicked buttons' background will be changed and disabled to prevent further unintentionally clicks
+![img.png](img.png)![img_1.png](img_1.png)
+  
+3.Game over
+- If you find the hidden word successfully, you will receive a alert message 🎉! 
+  And touch the space outside the alert dialog, the dialog will disappear.
+- If you fail after 10 attempts, the picture will be a completed hangman :( but also you could see what exactly the word is.
+  And you will receive a toast message. 
+- Besides, when the game overs, the restart button will be visible, after clicking that you will begin with new word to guess.
+![img_2.png](img_2.png)![img_3.png](img_3.png)
--- app/src/main/java/fr/isep/ii3510/assignment2__hangman_kyleliuxianqi/MainActivity.java
+++ app/src/main/java/fr/isep/ii3510/assignment2__hangman_kyleliuxianqi/MainActivity.java
@@ -1,14 +1,185 @@
 package fr.isep.ii3510.assignment2__hangman_kyleliuxianqi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @Author: LIU Xianqi 61551
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    private String word;
+    private int counter;
+    private char[] wordArray;
+    private Map<String,String> wordMap;
+    TextView tv_word, tv_clue, tv_counter;
+    ImageView iv_hangman;
+    Button btn_restart;
+    LinearLayout ll_keyboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_word = findViewById(R.id.tv_word);
+        tv_clue = findViewById(R.id.tv_clue);
+        tv_counter = findViewById(R.id.tv_counter);
+        btn_restart = findViewById(R.id.btn_restart);
+        iv_hangman = findViewById(R.id.iv_hangman);
+        iv_hangman.setImageResource(R.drawable.hangman10);
+
+        ll_keyboard = findViewById(R.id.keyboard);
+
+        //Use string array to get each line's values on the keyboard
+        String[] kb_line1_val = getResources().getStringArray(R.array.keyboard_line1_val);
+        String[] kb_line2_val = getResources().getStringArray(R.array.keyboard_line2_val);
+        String[] kb_line3_val = getResources().getStringArray(R.array.keyboard_line3_val);
+
+        createKeyboard(ll_keyboard, kb_line1_val, kb_line1_val.length);
+        createKeyboard(ll_keyboard, kb_line2_val, kb_line2_val.length);
+        createKeyboard(ll_keyboard, kb_line3_val, kb_line3_val.length);
+
+        readFile();
+        gameStart();
     }
+
+    //Read the words and clues from csv file
+    private void readFile() {
+        Scanner sc = new Scanner(getResources().openRawResource(R.raw.words_and_clues));
+        if (wordMap == null) {
+            wordMap = new HashMap<>();
+        }
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            //Due to the csv format as [word,clue], so use ',' as the index to get substring as word and clue
+            String word = line.substring(0, line.indexOf(','));
+            String clue = line.substring(line.indexOf(',')+1);
+            wordMap.put(word.toUpperCase(), clue);
+        }
+        sc.close();
+    }
+
+    public void gameStart(){
+        iv_hangman.setImageResource(R.drawable.hangman10);
+        word = getRandomWord();
+        wordArray = word.toCharArray();
+
+        //Build a hidden string which has the same length as the word
+        StringBuilder wordHidden = new StringBuilder();
+        for(int i = 0; i < word.length(); i++){
+            if(wordArray[i] == ' '){
+                wordHidden.append(" "); //some word like 'horse riding' contain a blank space
+            } else {
+                wordHidden.append("_");
+            }
+        }
+
+        tv_word.setText(wordHidden.toString());
+        tv_clue.setText(String.format(getString(R.string.clue), wordMap.get(word)));
+        counter = 10; //User has 10 lives at the beginning
+    }
+
+    public void createKeyboard(LinearLayout ll, String[] kb_line_val, int kb_line_val_num){
+        //create line
+        LinearLayout kb_line = new LinearLayout(this);
+        LinearLayout.LayoutParams kb_line_lp = new LinearLayout.LayoutParams(-1,-2,0);
+        kb_line.setBackgroundColor(ContextCompat.getColor(this,R.color.light_black));
+        ll.addView(kb_line, kb_line_lp); //Add the line for keyboard
+
+        //create btn
+        for(int i=0; i<kb_line_val_num; i++){
+            Button btn_key = new Button(this);
+            LinearLayout.LayoutParams btn_key_lp = new LinearLayout.LayoutParams(-2,-2,1);
+            btn_key.setText(kb_line_val[i]);
+            btn_key.setOnClickListener(this::onClickKeyboard);
+            kb_line.addView(btn_key,btn_key_lp);//Add the keyboard button for the line
+        }
+    }
+
+    public void onClickKeyboard(View v){
+
+        Button btn = (Button) v;
+        String input = btn.getText().toString();
+        StringBuilder str = new StringBuilder();
+        char letter = input.charAt(0);
+
+        if(word.contains(input)){
+            char[] wordHiddenArray = tv_word.getText().toString().toCharArray();
+            for(int i = 0; i < wordArray.length; i++){
+                if(wordArray[i] == letter){
+                    wordHiddenArray[i] = letter;
+                }
+                str.append(wordHiddenArray[i]);
+            }
+
+            //change background and be disabled to prevent further unintentionally clicks
+            btn.setEnabled(false);
+            btn.setBackgroundColor(getColor(R.color.light_black));
+            tv_word.setText(str.toString());
+
+            if(str.toString().equals(word)){
+                //Use alert dialog to display successful message
+                new AlertDialog.Builder(this)
+                        .setTitle("🎉Congratulations!")
+                        .setMessage("😊Amazing! You found the word!")
+                        .create().show();
+                gameOver();
+            }
+        }else{
+            if(counter>0){
+                counter--;
+                tv_counter.setText(String.format(getString(R.string.count),counter));
+
+                //when user lose one life, then exchange the img
+                int imgId = getResources().getIdentifier("hangman" + counter, "drawable", getPackageName());
+                iv_hangman.setImageResource(imgId);
+
+                //change background and be disabled to prevent further unintentionally clicks
+                btn.setEnabled(false);
+                btn.setBackgroundColor(Color.RED);
+            }
+            if(counter==0){
+                //Use toast to display failed message
+                Toast.makeText(this, "😭Sorry! You didn't find the word! Try again!", Toast.LENGTH_LONG).show();
+                gameOver();
+                tv_word.setText(word);
+            }
+        }
+    }
+
+    //randomly choose a word with its clue in this map
+    private String getRandomWord(){
+        Random random = new Random();
+        String[] keys = wordMap.keySet().toArray(new String[0]);
+        return keys[random.nextInt(keys.length)];
+    }
+
+    private void gameOver(){
+        //When user passed or failed the game, set the restart button visible
+        btn_restart.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickRestart(View v) {
+        finish();
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
+    }
+
 }
